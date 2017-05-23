f637ed53a4920c3f7bd653afe2f06bae12590706
TseHang <a54383813@gmail.com>
2017-05-23 23:01:05 +0800

    test merge ching

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,23 @@
-"use strict";!function(o){var n=!1;$("#logo").on("mouseover",function(){$(".logo-speak").css("opacity",1)}),$("#logo").on("mouseleave",function(){$(".logo-speak").css("opacity",0)}),$("#logo").on("click",function(){d3.select("#logo").attr("class",function(){return n=!n,n?"invert":""})})}();
+'use strict';
+
+// require('./partial/default.js');
+(function (window) {
+  var triggerLogo = false;
+  $('#logo').on('mouseover', function () {
+    $('.logo-speak').css('opacity', 1);
+  });
+
+  $('#logo').on('mouseleave', function () {
+    $('.logo-speak').css('opacity', 0);
+  });
+
+  $('#logo').on('click', function () {
+    d3.select('#logo').attr('class', function () {
+      triggerLogo = !triggerLogo;
+      if (triggerLogo) {
+        return 'invert';
+      }
+      return '';
+    });
+  });
+})();
--- dist/js/partial/default.js
+++ dist/js/partial/default.js
@@ -1,1 +1,89 @@
-"use strict";var isMobile=$(window).width()<768,formatFloat=function(e,o){var s=Math.pow(10,o);return Math.round(e*s)/s};!function(e){var o=0,s=!1,n=0,a=e.location.pathname;(a.includes("/index")||"/"===a||"/vp-tainan/"===a)&&(s=!0,n=$(".hot-bar").offset().top),isMobile?$("body").on({touchmove:function(e){var a=$(this).scrollTop();a<80||a<o?$(".nav").removeClass("hide-up"):($(".nav").addClass("hide-up"),$(".m-menu-list").removeClass("open"),$("#m-menu-button").removeClass("open")),s&&(a>=n-10?($(".hot-bar").addClass("fixed"),$(".news-right").addClass("fixed")):($(".hot-bar").removeClass("fixed"),$(".news-right").removeClass("fixed"))),o=a}}):$(document).scroll(function(){var a=$(e).scrollTop();a<80||a<o?$(".nav").removeClass("hide-up"):($(".nav").addClass("hide-up"),$(".m-menu-list").removeClass("open"),$("#m-menu-button").removeClass("open")),s&&(a>=n-10?($(".hot-bar").addClass("fixed"),$(".news-right").addClass("fixed")):($(".hot-bar").removeClass("fixed"),$(".news-right").removeClass("fixed"))),o=a}),$("#m-menu-button").click(function(e){$(this).toggleClass("open"),$(".m-menu-list").toggleClass("open")}),$(".m-menu-list>a").click(function(e){$("#m-menu-button").removeClass("open"),$(".m-menu-list").removeClass("open")}),$(".collapse-toggle").click(function(e){e.preventDefault();var o=$(this).data("id");$("#"+o).slideToggle(200)})}(window);
+'use strict';
+
+var isMobile = $(window).width() < 768 ? true : false;
+
+// Global Func
+var formatFloat = function formatFloat(num, pos) {
+  var size = Math.pow(10, pos);
+  return Math.round(num * size) / size;
+};
+
+(function (window) {
+  var lastScrollY = 0;
+  var isIndex = false;
+  var fixHotBarTop = 0;
+  var pathname = window.location.pathname;
+
+  // include github's location state
+  if (pathname.includes('/index') || pathname === '/' || pathname === '/vp-tainan/') {
+    isIndex = true;
+    fixHotBarTop = $('.hot-bar').offset().top;
+  }
+
+  // For mobile
+  if (isMobile) {
+    $('body').on({
+      touchmove: function touchmove(e) {
+        var st = $(this).scrollTop();
+        if (st < 80 || st < lastScrollY) {
+          $('.nav').removeClass('hide-up');
+        } else {
+          $('.nav').addClass('hide-up');
+          $('.m-menu-list').removeClass('open');
+          $('#m-menu-button').removeClass('open');
+        }
+
+        if (isIndex) {
+          if (st >= fixHotBarTop - 10) {
+            $('.hot-bar').addClass('fixed');
+            $('.news-right').addClass('fixed');
+          } else {
+            $('.hot-bar').removeClass('fixed');
+            $('.news-right').removeClass('fixed');
+          }
+        }
+        lastScrollY = st;
+      }
+    });
+  } else {
+    $(document).scroll(function () {
+      var st = $(window).scrollTop();
+
+      if (st < 80 || st < lastScrollY) {
+        $('.nav').removeClass('hide-up');
+      } else {
+        $('.nav').addClass('hide-up');
+        $('.m-menu-list').removeClass('open');
+        $('#m-menu-button').removeClass('open');
+      }
+
+      if (isIndex) {
+        if (st >= fixHotBarTop - 10) {
+          $('.hot-bar').addClass('fixed');
+          $('.news-right').addClass('fixed');
+        } else {
+          $('.hot-bar').removeClass('fixed');
+          $('.news-right').removeClass('fixed');
+        }
+      }
+      lastScrollY = st;
+    });
+  }
+
+  $('#m-menu-button').click(function (e) {
+    $(this).toggleClass('open');
+    $('.m-menu-list').toggleClass('open');
+  });
+
+  $('.m-menu-list>a').click(function (e) {
+    $('#m-menu-button').removeClass('open');
+    $('.m-menu-list').removeClass('open');
+  });
+
+  // Fix semantic-ui's click's jump
+  $('.collapse-toggle').click(function (e) {
+    e.preventDefault();
+    var id = $(this).data('id');
+    $('#' + id).slideToggle(200);
+  });
+})(window);
