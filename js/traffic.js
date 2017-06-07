4e8bc9ede0f3c1ea23f3fff430006b760bfb8d34
peihsuan1225 <peihsuan1225@gmail.com>
2017-06-07 17:44:27 +0800

    add legend and toggles

--- js/traffic.js
+++ js/traffic.js
@@ -1,67 +1,78 @@
 //  map of traffic events
-const mymap = L.map('accidentMap').setView([22.98877766803278, 120.21154403686522], 14)
-
+// initial map
+const mymap = L.map('accidentMap').setView([22.9945, 120.21208], 14)
 L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
   maxZoom: 18,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy;<a href="https://carto.com/attribution">CARTO</a>',
 }).addTo(mymap)
 
-// ./src/data/traffic10506.csv
+// add legend
+const legend = L.control({ position: 'bottomright' })
 
-function addEventCircle(event) {
+legend.onAdd = () => {
+  const div = L.DomUtil.create('div', 'info legend'),
+    colors = ['#ff6f69', '#265665'],
+    labels = ['白天(5:00-17:00)', '晚上(17:00-5:00)']
+
+  for (let i = 0; i < labels.length; i++) {
+    div.innerHTML +=
+    '<p><i style="background:' + colors[i] + '"></i> ' + labels[i] + '<br></p>'
+  }
+  return div
+}
+legend.addTo(mymap)
+
+// toggle onclick
+$('#day_toggle').on('click', () => {
+  let isChecked = $('#day_toggle').is(':checked')
+    if(isChecked) {
+      $('.day').css('visibility' , 'visible')
+    } else {
+      $('.day').css('visibility' , 'hidden')
+    }
+})
+$('#night_toggle').on('click', () => {
+  let isChecked = $('#night_toggle').is(':checked')
+    if(isChecked) {
+      $('.night').css('visibility' , 'visible')
+    } else {
+      $('.night').css('visibility' , 'hidden')
+    }
+})
+
+function addEventCircle(event, map) {
   const lat = event.lat
   const lng = event.lng
-  const month = event.month
-  const daynight = (event.hour < 17 && event.hour > 5) ? ('day') : ('night')
-  const date = event.month + '月' + event.day + '日'
+  const daynight = event.daynight
+  const date = event.date
   if (lat !== undefined || lng !== undefined) {
     if (daynight === 'day') {
       const circle = L.circle([lat, lng], {
         color: '#ff6f69',
-        fillColor: '#f03',
-        fillOpacity: 0.1,
-        radius: 0.2,
-        class: month,
-      }).addTo(mymap)
-      circle.bindPopup(date + ' lat: '+ lat + 'lng' + lng)
+        stroke: false,
+        fillColor: '#ff6f69',
+        fillOpacity: 0.5,
+        radius: 15,
+        className: 'circle day',
+      }).addTo(map)
+      circle.bindPopup('時間： ' + date.toString())
     } else {
       const circle = L.circle([lat, lng], {
         color: '#265665',
-        fillColor: '#f03',
-        fillOpacity: 0.1,
-        radius: 0.2,
-        class: month,
-      }).addTo(mymap)
-      circle.bindPopup(date + ' lat: '+ lat + 'lng' + lng)
+        stroke: false,
+        fillColor: '#265665',
+        fillOpacity: 0.5,
+        radius: 15,
+        className: 'circle night',
+      }).addTo(map)
+      circle.bindPopup('時間： ' + date.toString())
     }
   }
 }
 
-function processCSVData(allText) {
-  const allTextLines = allText.split(/\r\n|\n/)
-  const headers = allTextLines[0].split(',')
-  const lines = []
-  for (let i = 1; i < allTextLines.length; i++) {
-    const data = allTextLines[i].split(',')
-    console.log( data[58] + ' '  + data[59])
-    const EventData = { lat: data[59], lng: data[58], category: 'injury', month: data[5], day: data[6], hour: data[7] }
-    addEventCircle(EventData)
- /*   if (data.length === headers.length) {
-      const tarr = []
-      for (let j = 0; j < headers.length; j++) {
-        tarr.push(headers[j] + ':' + data[j])
-      }
-      lines.push(tarr)
-    }
-  }*/
-  }
-}
-$(document).ready(function() {
-  $.ajax({
-    type: 'GET',
-    url: './src/data/traffic10506.csv',
-    dataType: 'text',
-    success: function (data) { processCSVData(data)},
+$.getJSON('./src/data/trafficeRawData.json', (data) => {
+  $.each(data, (index, value) => {
+    addEventCircle(value, mymap)
   })
 })
 // pie chart code (d3)
