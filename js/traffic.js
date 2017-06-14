686e4dde336382fa50fbb37d7bddc6331d01a9f8
tinglo <tinglo@tinglode-MacBook-Pro.local>
2017-06-14 13:18:15 +0800

    modify content

--- js/traffic.js
+++ js/traffic.js
@@ -75,20 +75,6 @@ $.getJSON('./src/data/trafficeRawData.json', (data) => {
     addEventCircle(value, mymap)
   })
 });
-// pie chart code (d3)
-/*
-let svg = d3.select('svg')
-.append('g')
-.attr('class', 'slices')
-.attr('class', 'labels')
-.attr('class', 'lines')
-
-const width = $('svg').width()
-const height = $('svg').height()
-const radius = Math.min(width, height / 2)
-*/
-// table next to pie chart
-
 
 
 
@@ -96,11 +82,8 @@ const radius = Math.min(width, height / 2)
 //total event 356590 + 12401 +1049
 //important event 352617
 (function(d3) {
- // var svgWidth = 300;
   var svgHeight = 300;
-  //var totalWidth = svgWidth + 300;
-  var totalHeight = svgHeight + 150;
-  //var radius = Math.min(svgWidth, svgHeight) / 2;
+  var totalHeight = svgHeight + 120;
   var radius = svgHeight / 2;
   var recSize = 18;
   var thickness = 25;
@@ -112,13 +95,7 @@ const radius = Math.min(width, height / 2)
       .append('svg')
       .attr('width', '100%')
       .attr('height', totalHeight);
-      //.append('g')
-      // .attr('display', 'block')
-      // .attr('margin', 'auto');
-      //.attr('transform', 'translate(' + 300 + ',' + (svgHeight * 2 / 3) + ')')
-      //.attr('transform', 'translate(' + $('svg').width() / 2  + ',' + (svgHeight * 2 / 3) + ')');
       
-
   var arc = d3.svg.arc()
       .innerRadius(radius - thickness)
       .outerRadius(radius);
@@ -128,7 +105,6 @@ const radius = Math.min(width, height / 2)
                 .attr('id', 'pieSvg')
                 .attr('class', 'pie-svg')
                 .attr('transform', 'translate(' + $('svg').width() / 2  + ',' + (svgHeight * 2 / 3 + 10)  + ')');
-                //.attr('display', 'none');
 
   pieSvg.append('g')
       .append('circle')
@@ -161,7 +137,6 @@ const radius = Math.min(width, height / 2)
     .attr('class', 'percent'); 
 
   d3.csv('./src/data/trafficExposedData.csv', function(error, data) {
-    //var color = d3.scale.category20c();
 
     var colorLabel = [];
 
@@ -169,13 +144,8 @@ const radius = Math.min(width, height / 2)
       colorLabel.push(d.event);
     });
     var color = d3.scale.ordinal()
-                //.domain(['未領有駕照駕車','未戴安全帽','酒精濃度超過規定標準者','行車速度超速60公里以下','爭道行駛','不依規定轉彎或變換車道','闖紅燈直行左轉','闖紅燈右轉','違規臨時停車','違規停車','停車時間位置方式車種不依規定','併排停車','其他不遵守標誌標線號誌駕車','違規停車拖吊'])
-                //.domain([1, 14])
-                //.range(['#FFB6C1', '#66CDAA', '#87CEFA', '#48D1CC', '#7B68EE', '#F4A460', '#ADD8E6', '#CA8EC2', '#81C0C0', '#8080C0', '#FFBB77', '#AAAAFF', '#FF79BC', '#FFAD86']);
                 .domain(colorLabel)
                 .range(['#4F9D9D', '#95CACA', '#5A5AAD', '#A6A6D2', '#C07AB8', '#FFAD86', '#FF95CA', '#00E3E3', '#009393', '#46A3FF', '#66CDAA', '#8600FF', '#CA8EFF', '#FFB5B5'])
-                //.range(['#4169E', '#1E90FF', '#ADD8E6', '#40E0D0', '#87CECB', '#FFDAB9', '#FFA07A', '#CD5C5C', '#D87093', '#FF69B4', '#FFB6C1', '#DDA0DD', '#9370DB', '#6A5ACD']);
-
 
     data.forEach(function(d) {
       d.times = +d.times;
@@ -215,7 +185,7 @@ const radius = Math.min(width, height / 2)
     });
 
     /* label */
-    var g = pieSvg.append("g").attr("transform", "translate(" + 0/ 2 + "," + 0/ 2 + ")");
+    var g = pieSvg.append('g').attr('transform', 'translate(' + 0 + ',' + 0 + ')');
 
     var radius2 = (svgHeight + 100) / 2;
 
@@ -232,23 +202,24 @@ const radius = Math.min(width, height / 2)
       return d.startAngle + (d.endAngle - d.startAngle) / 2;
     }
 
-    g.selectAll("text").data(pie(data))
+    g.selectAll('text').data(pie(data))
       .enter()
-      .append("text")
-      .attr("class", "label-style")
-      .attr("transform", function(d){
+      .append('text')
+      .attr('class', 'label-style')
+      .attr('fill', '#3C3C3C')
+      .attr('transform', function(d){
 
         var pos = outerArc2.centroid(d);
         pos[0] = radius2 * (midAngle(d) < Math.PI ? 1 : -1);
-        return "translate("+ pos +")";
+        return 'translate('+ pos + ')';
       })
-      .attr("text-anchor", function(d){
+      .attr('text-anchor', function(d){
         var direction = midAngle(d) < Math.PI ? 1 : -1;
         if(direction === -1){
-          return "end";
+          return 'end';
         }
         else{
-          return "start";
+          return 'start';
         }
       })
       .text(function(d) {
@@ -260,13 +231,13 @@ const radius = Math.min(width, height / 2)
       });
     /* polyline */
 
-    var polyline = g.selectAll("polyline")
+    var polyline = g.selectAll('polyline')
       .data(pie(data), function(d) {
         return d.data.event;
       })
       .enter()
-      .append("polyline")
-      .attr("points", function(d) {
+      .append('polyline')
+      .attr('points', function(d) {
         var pos = outerArc2.centroid(d);
             pos[0] = radius2 * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
             
@@ -310,20 +281,6 @@ const radius = Math.min(width, height / 2)
         var percent = Math.round(1000 * d.times / total) / 10;
         return d.event + ' ' + percent + '%'; 
       });
-
-    //$( window ).resize(function() {
-      
-
-      // if(!isMobile){
-      //   $('#pieSvg').show();
-      //   $('#legendSvg').hide();
-      // }
-      // if(isMobile){
-      //   $('#pieSvg').hide(); 
-      //   $('#legendSvg').show();     
-      // }
-   // });
-
   });
 })(window.d3);
 
